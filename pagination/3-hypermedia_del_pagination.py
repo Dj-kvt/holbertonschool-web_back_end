23315e9bfe48635a2c40e5c12973312b9926d6f0
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-08-14 15:00:31 +0200

    correct pycodestyle error of my file 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -53,10 +53,11 @@ class Server:
             current_index += 1
             # Skip any missing index due to deletion
             while current_index not in indexed_data and current_index < max(
-                indexed_data.keys()) + 1:
+                    indexed_data.keys()) + 1:
                 current_index += 1
 
-        next_index = current_index if current_index < max(indexed_data.keys()) + 1 else None
+        next_index = current_index if current_index < max(
+            indexed_data.keys()) + 1 else None
 
         return {
             'index': index,
