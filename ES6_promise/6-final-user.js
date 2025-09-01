ee06d3c80784a338d811400c348d38ff8e8b53d4
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-09-01 16:48:08 +0200

    correct the output of my return table

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,16 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-    firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
-  return Promise.allSettled([userPromise, photoPromise]);
+
+  const results = await Promise.allSettled([userPromise, photoPromise]);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return { status: 'fulfilled', value: result.value };
+    }
+    return { status: 'rejected', value: result.reason.toString() };
+  });
 }
