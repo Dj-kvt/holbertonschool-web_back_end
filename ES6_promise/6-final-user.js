9ad77bef3340b1c853b4b66d8fc5d8334c1ded86
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-09-01 16:43:38 +0200

    correct eslint error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,8 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(
+    firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
   return Promise.allSettled([userPromise, photoPromise]);
