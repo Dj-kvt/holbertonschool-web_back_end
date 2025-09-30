ec3bcd5f80bfc55c16b5203fd3217f74cf4096a7
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-09-30 02:19:55 +0200

    correct the error version command of the files five and six

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,16 +7,14 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
+    total_logs = collection.count()
     print(f"{total_logs} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = collection.count_documents({"method": method})
+        count = collection.count({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    status_check = collection.count({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
